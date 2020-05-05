4836cd9d0db0637b081011cd1058dc67489cf161
Your Name <you@example.com>
2020-05-05 15:43:53 +0000

    update wsgi

--- djtrump/wsgi.py
+++ djtrump/wsgi.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+import sys
+
+sys.path.append('/home/abcd/www/djtrump')
+
+sys.path.append('/home/abcd/www/venv/lib/python3.7/site-packages')
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djtrump.settings")
